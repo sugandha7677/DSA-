eb43c15e159e20f13021dabb4c523676793a04e1
sugandha tiwary <98490267+sugandha7677@users.noreply.github.com>
2023-03-23 21:43:08 +0530

    Delete BinaryTree23-03-23 directory

--- BinaryTree23-03-23/PreOrder.java
+++
@@ -1,9 +0,0 @@
-void preOrder(Node root) {
-    if(node == root) return;
-
-    System.out.print(root.data + " ");
-    preOrder(root.left);
-    preOrder(root.right);
-}
-
-//TC: O(n) n = no. of nodes
