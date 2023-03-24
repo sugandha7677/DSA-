723e04dc0eefe8cbd25143cae982683ed23c02d4
sugandha tiwary <98490267+sugandha7677@users.noreply.github.com>
2023-03-24 16:28:46 +0530

    Update PreOrder.java

--- BinaryTree23-03-23/Algorithms/PreOrder.java
+++ BinaryTree23-03-23/Algorithms/PreOrder.java
@@ -1,5 +1,7 @@
+// Recursive solution
+
 void preOrder(Node root) {
-    if(node == root) return;
+    if(root == null) return;
 
     System.out.print(root.data + " ");
     preOrder(root.left);
@@ -7,3 +9,26 @@ void preOrder(Node root) {
 }
 
 //TC: O(n) n = no. of nodes
+
+
+// Iterative way
+// use stack of Node , store root , pop & print and then push first its right child then left
+
+void preOrder(Node root) {
+    if(noot == null ) return;
+    
+    Stack<Node> st = new Stack<>();
+    st.push(root);
+
+    while(!st.isEmpty()) {
+        Node top = st.peek();
+        System.out.println(st.pop);
+    
+        if(top.right != null) st.push(top.right);
+        if(top.left != null) st.push(top.left); 
+    }
+    
+}   
+
+// Tc: O(n)
+// SC: O(n) or O(height of BT)
