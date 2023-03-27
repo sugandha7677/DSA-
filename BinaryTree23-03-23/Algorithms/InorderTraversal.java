b2c6e87492b521c2992ffb79e1bb11c3a9127a32
sugandha tiwary <98490267+sugandha7677@users.noreply.github.com>
2023-03-28 03:07:14 +0530

    Update InorderTraversal.java

--- BinaryTree23-03-23/Algorithms/InorderTraversal.java
+++ BinaryTree23-03-23/Algorithms/InorderTraversal.java
@@ -10,3 +10,31 @@ public class InorderTraversal {
 }
 
 //TC: O(n)
+
+// Iterative way
+// push root into node -> go to left -> if(null) -> pop, print and go to right
+//            else push in stack and move to left
+
+void inOrder(Node root){
+    Stack<Node> st = new Stack<>();
+
+    Node node = root;
+    while(true){
+        if(node != null){
+            st.push(node);
+            node = node.left;
+        }else{
+            if(st.IsEmpty()){
+                break;
+            }
+            
+            Node n = st.pop();
+            System.out.println(n.data);
+            node = node.right;
+        }
+    }
+}
+
+//TC: O(n) height of tree
+// why ?? take case having left node only, it goes on pushing and node.left
+
