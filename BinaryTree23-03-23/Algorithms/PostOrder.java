fec02305ae1111cbb292e77d84c1fdbe674645c8
sugandha tiwary <98490267+sugandha7677@users.noreply.github.com>
2023-03-28 11:32:33 +0530

    Update PostOrder.java

--- BinaryTree23-03-23/Algorithms/PostOrder.java
+++ BinaryTree23-03-23/Algorithms/PostOrder.java
@@ -1,3 +1,5 @@
+// Recursive Approach
+
 public class PostOrder {
     void postOrder(Node root) {
         if(root == null) return;
@@ -9,3 +11,35 @@ public class PostOrder {
 }
 
 // TC: O(n)
+
+// Using 2 stack
+
+void postOrder(Node root){
+    Stack<Node> s1 = new Stack<>();
+    Stack<Node> s2 = new Stack<>();
+    
+    if(root == null) return;
+    s1.push(root);
+    
+    while(!s1.isEmpty()){
+        Node node = s1.peek();
+        s1.pop();
+        s2.push(node);
+        
+        if(node.left != null) s1.push(node.left);
+        if(node.right != null) s2.push(node.right);
+    }
+    
+    while(!s2.isEmpty()){
+        System.out.println(s2.pop());
+    }
+}
+
+// TC: O(n)
+// SC: O(2n)
+
+
+// Using single Stack
+
+
+
