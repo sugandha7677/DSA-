f85a654b2f2a79f682fdc054e63f0620049f518f
sugandha tiwary <98490267+sugandha7677@users.noreply.github.com>
2023-05-14 23:08:11 +0530

    l1

---
+++ Heap/Intro.java
@@ -0,0 +1,7 @@
+// in java, we have by default min heap for max heap we use comparator
+    
+    // Max heap
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> b - a);
+
+    // min heap
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>(); 
